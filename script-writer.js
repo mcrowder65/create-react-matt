980527c0e335c1382c0b51928f3be70e1f20bb0a
Matt Crowder <mcrowder65@gmail.com>
2017-11-18 11:15:31 -0600

    scripters

--- script-writer.js
+++ script-writer.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 const pkg = require("./package.json");
 pkg.scripts.start = "webpack-dev-server";
-pkg.scripts.test = "karma start";
+pkg.scripts.test = "jest --coverage";
+pkg.jest = {
+    "collectCoverageFrom": [
+        "src/**/*.{js*}",
+        "!src/client/app.jsx",
+        "!src/client/router.jsx"
+    ]
+};
 
 fs.writeFile("./package.json", JSON.stringify(pkg));
