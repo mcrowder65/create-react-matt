8e0d0a0181ef500b3ff17a5a8fb95ea747e46fdc
Matt Crowder <matt.crowder@walmart.com>
2018-06-01 14:56:35 -0400

    promise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 #!/usr/bin/env node
 const program = require("commander");
+const { exec } = require("child_process");
 
+const executeCommand = (command, output) => {
+  return new Promise((resolve, reject) => {
+    try {
+      exec(command, (error, stdout) => {
+        if (error) {
+          reject(error);
+        } else {
+          if (output) {
+            console.log(output);
+          }
+          resolve(stdout);
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 program
   .arguments("<folder>")
-  .action(folder => {
-    console.log(folder);
+  .action(async folder => {
+    try {
+      await executeCommand(`mkdir ${folder}`, `Created ${folder}`);
+      await executeCommand(`cd ${folder}`, `cd'd into ${folder}`);
+
+
+    } catch (error) {
+      console.log("Something went wrong, sorry");
+    }
   })
   .parse(process.argv);
