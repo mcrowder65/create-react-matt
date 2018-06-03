455a89603755295a319cc466753cc95d899dea8c
Matt Crowder <matt.crowder@walmart.com>
2018-06-03 19:21:08 -0400

    script for master

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo $TRAVIS_PULL_REQUEST_BRANCH
-npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#$TRAVIS_PULL_REQUEST_BRANCH
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+    echo 'installing '$TRAVIS_PULL_REQUEST_BRANCH
+    npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#$TRAVIS_PULL_REQUEST_BRANCH
+else
+    echo 'installing master'
+    npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#master
+fi
