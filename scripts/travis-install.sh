c2dc77a2d4ee04e722507eaa8eace0074e23a041
Matt Crowder <matt.crowder@walmart.com>
2018-06-03 17:59:06 -0400

    should work now

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $TRAVIS_BRANCH
-npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#$TRAVIS_BRANCH
+echo $TRAVIS_PULL_REQUEST_BRANCH
+npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#$TRAVIS_PULL_REQUEST_BRANCH
