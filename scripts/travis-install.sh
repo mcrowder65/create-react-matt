f35b824406ba7b71b64562cd98c2faf9a04c48a3
Matt Crowder <matt.crowder@walmart.com>
2018-06-03 19:26:48 -0400

    ok this should work

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo 'travis_pull_request'$TRAVIS_PULL_REQUEST
-if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
-    echo 'installing '$TRAVIS_PULL_REQUEST_BRANCH
-    npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#$TRAVIS_PULL_REQUEST_BRANCH
-else
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo 'installing master'
     npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#master
+else
+    echo 'installing '$TRAVIS_PULL_REQUEST_BRANCH
+    npm install -g create-react-matt@https://github.com/mcrowder65/create-react-matt#$TRAVIS_PULL_REQUEST_BRANCH
 fi
