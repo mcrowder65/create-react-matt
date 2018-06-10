8848e171fa9d0e039fd2f0d04e3c802e7a4277f1
Matt Crowder <matt.crowder@walmart.com>
2018-06-10 18:31:00 -0400

    more coverage

--- build-index.js
+++ build-index.js
@@ -110,6 +110,7 @@ program.arguments("<folder>").option("-y, --yarn", "Use yarn").option("-f, --for
                       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
                       "\\.(css|scss|less)$": "identity-obj-proxy"
                     },
+                    modulePaths: ["src/client/"],
                     coverageReporters: ["html"]
                   })
 
