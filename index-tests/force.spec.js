cf9ec14609edd6002dbfada284349f259d6fdb3b
Matt Crowder <matt.crowder@walmart.com>
2018-06-13 10:11:07 -0400

    forcef

--- index-tests/force.spec.js
+++ index-tests/force.spec.js
@@ -6,18 +6,7 @@ test("When force exists, it should reject", async () => {
     await executeBashFunction(`mkdir ${folder}`);
     process.argv.push(folder);
     await cli();
-
   } catch (e) {
     expect(e.message.indexOf("file already exists")).toBeGreaterThan(-1);
   }
-});
-
-test("When force exists and you pass -f, it should delete it and work!", async () => {
-  const folder = "forcef";
-  await executeBashFunction(`mkdir ${folder}`);
-  process.argv.push(folder);
-  process.argv.push("-f");
-  process.argv.push("-s");
-  await cli();
-  expect(await doesFileExist(folder)).toBeTruthy();
 });
